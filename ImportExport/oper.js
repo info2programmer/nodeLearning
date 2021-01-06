f1169ad12012a49c0dfdb20c7c9e52d3413d6011
Saikat Bhadury <info2programmer@gmail.com>
2021-01-06 12:18:46 +0530

    Import And Export Implement

---
+++ ImportExport/oper.js
@@ -0,0 +1,13 @@
+const add = (a, b) => {
+    return a + b
+}
+
+const sub = (a, b) => {
+    return a - b
+}
+
+const name = "saikat"
+
+// module.exports.add = add
+// module.exports.sub = sub
+module.exports = { add, sub , name}

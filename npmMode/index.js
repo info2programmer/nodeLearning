7415e8457a180b85f7c7c6c5f8cb913c73279613
Saikat Bhadury <info2programmer@gmail.com>
2021-01-16 11:31:24 +0530

    npm module implement

--- npmMode/index.js
+++ npmMode/index.js
@@ -6,3 +6,13 @@ console.log(chalk.blue("Hello World!"))
 const res = validator.isEmail("info2programmer@gmail.com")
 
 console.log(res ? chalk.greenBright.inverse(res) : chalk.redBright.inverse(res))
+
+// const foo =()=>{
+//     let a = b = 10
+//     a++;
+//     return a;
+// }
+// foo()
+
+// console.log(a)
+// console.log(b)
